166a28d935242d526c3c6af6cca0dccc205de157
Nagib Mahfuz <120543584+nagibgithub@users.noreply.github.com>
2023-05-29 20:04:34 +0600

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div className='min-h-screen flex justify-center items-center'>
+            This is home            
+        </div>
+    );
+};
+
+export default Home;
